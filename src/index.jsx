ea43920495343550a8b12c923943e697192759de
Sergio R <siarhei.rudak@gmail.com>
2021-09-01 12:47:28 +0300

    progress

---
+++ src/index.jsx
@@ -0,0 +1,40 @@
+import React, { useState, useEffect } from 'react';
+import ReactDOM from 'react-dom';
+import { ToastContainer } from 'react-toastify';
+import Moralis from 'moralis';
+import { getConfig } from './config';
+import SettingsContext from './SettingsContext';
+import App from './components/App';
+
+import 'modern-css-reset/dist/reset.min.css';
+import 'react-toastify/dist/ReactToastify.css';
+import './index.css';
+// import "@pathofdev/react-tag-input/build/index.css";
+
+function AppContextWrap() {
+  const [settingsState, setSettingsState] = useState({
+    user: null
+  });
+
+  return (
+    <SettingsContext.Provider value={{ settingsState, setSettingsState }}>
+      <App/>
+      <ToastContainer
+        position="top-right"
+        autoClose={4000}
+        hideProgressBar={true}
+        newestOnTop={false}
+        closeOnClick
+        closeButton={false}
+        limit={3}
+      />
+    </SettingsContext.Provider>
+  );
+}
+
+ReactDOM.render(
+  <React.StrictMode>
+    <AppContextWrap/>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
